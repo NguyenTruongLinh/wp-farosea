acc8c496929969fbd855925be6389fcf722f7a06
linh <nhokkuteo1996@gmail.com>
2020-07-03 11:51:58 +0700

    Update single.php

--- wp-content/themes/farosea/single.php
+++ wp-content/themes/farosea/single.php
@@ -12,7 +12,7 @@ get_header();
 
 <?php while ( have_posts() ) :the_post(); ?>
 <div class="post-wrapper">
-    <div class="post-banner_img" style="background: url(<?php the_post_thumbnail_url() ?>); background-attachment: fixed; background-position: center top; background-repeat: no-repeat;">
+    <div class="post-banner_img" style="background: url(<?php the_post_thumbnail_url() ?>); background-attachment: fixed; background-position: center top; background-repeat: no-repeat; background-size: cover;">
         <h2><?php the_title(); ?></h2>
     </div>
     <div class="container-full">
