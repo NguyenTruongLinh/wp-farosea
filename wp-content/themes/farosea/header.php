1aae963dd406d3986b4f560d4928cc46f66a0453
linh <nhokkuteo1996@gmail.com>
2020-07-07 12:30:06 +0700

    Update header.php

--- wp-content/themes/farosea/header.php
+++ wp-content/themes/farosea/header.php
@@ -31,7 +31,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />       <!---->
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/js/lightgallery/css/lightgallery.min.css" />       <!---->
 
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/css/style.css?v=<?php echo date("YmdHm"); ?>" />
 
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/images/ico/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
